14a6db58cc506fc1ff550b2a6c6dffac55b47a73
Marcelo Lira <mlira@acumera.com>
2023-05-25 04:12:11 -0300

    [patch] bottomstack and bstackhoriz layouts.
    bstack        (TTT)       bstackhoriz   (===)
    +-----------------+       +-----------------+
    |                 |       |                 |
    |                 |       |                 |
    |                 |       |                 |
    +-----+-----+-----+       +-----------------+
    |     |     |     |       +-----------------+
    |     |     |     |       +-----------------+
    +-----+-----+-----+       +-----------------+
    
    Source: https://dwm.suckless.org/patches/bottomstack/

--- config.def.h
+++ config.def.h
@@ -46,6 +46,8 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
 };
 
 /* key definitions */
@@ -104,6 +106,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY|ShiftMask,             XK_o,      setlayout,      {.v = &layouts[6]} },
 	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
