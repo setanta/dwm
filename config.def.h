4601d2534948ba9a1c41ffc53d9be92a69f1eb6a
Marcelo Lira <mlira@acumera.com>
2023-05-28 23:48:04 -0300

    [patch] gapless grid layout
    This patch is an altered gridmode layout for dwm, which arranges the windows in a grid.
    Instead of using a regular grid, which might leave empty cells when there are not enough
    windows to fill the grid, it adjusts the number of windows in the first few columns
    to avoid empty cells.
    
    Source: https://dwm.suckless.org/patches/gaplessgrid/

--- config.def.h
+++ config.def.h
@@ -54,6 +54,7 @@ static const Layout layouts[] = {
 	{ ">M>",      centeredfloatingmaster },
 	{ "TTT",      bstack },
 	{ "===",      bstackhoriz },
+	{ "###",      gaplessgrid },
 	{ "[D]",      deck },
 	{ NULL,       NULL },
 };
@@ -128,7 +129,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY|ShiftMask,             XK_o,      setlayout,      {.v = &layouts[6]} },
-	{ MODKEY|ShiftMask,             XK_d,      setlayout,      {.v = &layouts[7]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[7]} },
+	{ MODKEY|ShiftMask,             XK_d,      setlayout,      {.v = &layouts[8]} },
 	{ MODKEY|ControlMask,           XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
